716c610cafcfc95618d65414b20ae796440070f2
pawelcichy <pawelcichy24@wp.pl>
2014-10-28 16:26:01 -0400

    cos

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,6 +4,13 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
+var ec2= AWS.ec2  ();
+var params = ();
+ec2.describeInstance (params, funvtion(err, data) {
+if (err) console.log(err, err.stack);
+else consle.log(data);
+})
+
 	callback(null, "Hello from ec2");
 	
 }
